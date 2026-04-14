e30a2d1a9dfaca48263305f2e2f8ce6d967501b5
Vasilovich <vasilovich.aleksandr@gmail.com>
2026-04-14 18:25:28 +0300

    New apply

--- infrastructure/modules.tf
+++ infrastructure/modules.tf
@@ -39,8 +39,6 @@ module "github_oidc_terraform" {
   github_repo                       = var.github_repo
   github_branch                     = var.github_branch
   github_oidc_provider_arn          = module.github_oidc_ecr.github_oidc_provider_arn
-  rds_master_secret_arn             = module.rds_postgres.master_user_secret_arn
-  openwebui_database_url_secret_arn = module.openwebui_database_url_secret.secret_arn
   common_tags                       = local.common_tags
 }
 
--- infrastructure/modules/github-oidc-terraform/vars.tf
+++ infrastructure/modules/github-oidc-terraform/vars.tf
@@ -18,12 +18,3 @@ variable "github_oidc_provider_arn" {
   description = "ARN of GitHub Actions OIDC provider"
   type        = string
 }
-
-variable "rds_master_secret_arn" {
-  description = "ARN of RDS master secret in Secrets Manager"
-  type        = string
-}
-
-variable "openwebui_database_url_secret_arn" {
-  type = string
-}
