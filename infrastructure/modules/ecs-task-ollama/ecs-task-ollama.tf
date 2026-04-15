060e882a99903c6111e18039f83c93a1f0079b19
Vasilovich <vasilovich.aleksandr@gmail.com>
2026-04-15 14:05:38 +0300

    Fix Config

--- infrastructure/modules/ecs-task-ollama/ecs-task-ollama.tf
+++ infrastructure/modules/ecs-task-ollama/ecs-task-ollama.tf
@@ -1,3 +1,12 @@
+resource "aws_cloudwatch_log_group" "ollama" {
+  name              = "/ecs/${var.project}-${var.env}-ollama"
+  retention_in_days = 7
+
+  tags = merge(var.common_tags, {
+    Name = "${var.project}-${var.env}-ollama-logs"
+  })
+}
+
 resource "aws_ecs_task_definition" "this" {
   family                   = "${var.project}-${var.env}-ollama"
   requires_compatibilities = ["FARGATE"]
@@ -25,6 +34,15 @@ resource "aws_ecs_task_definition" "this" {
           value = "0.0.0.0:11434"
         }
       ]
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = "/ecs/${var.project}-${var.env}-ollama"
+          awslogs-region        = var.aws_region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
     },
     {
       name      = "alloy"
@@ -47,7 +65,6 @@ resource "aws_ecs_task_definition" "this" {
         }
       }
     }
-
   ])
 
   tags = merge(var.common_tags, {
